ec660237a2c93fd7d5ce01d82c2f0e7a3d6d3d99
FelipeEmerim <lip.emerim@gmail.com>
2018-05-25 22:14:46 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const ip = require('ip');
 const app = express();
 
 app.use(express.static('public'));
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname+'/public/Battleships.html')));
-app.listen(3000, () => console.log('BattleShips server online at ip'+ip.address()+' port 3000!'));
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname + '/public/Battleships.html'));
+});
+
+
+app.all('/*',function(req,res){
+    res.redirect('/');
+
+});
+
+app.listen(3000, () => console.log('BattleShips server online at ip '+ip.address()+' port 3000!'));
